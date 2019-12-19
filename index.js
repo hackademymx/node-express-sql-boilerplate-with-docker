5291143a4c803218f1d3688cc626265741488860
oscar <oscaroceguera@gmail.com>
2019-12-18 19:25:13 -0700

    remove comments

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import server from './server';
 import db from './server/config/database';
-import models from './server/models';
 
 const PORT = process.env.PORT || 5000;
 
@@ -13,36 +12,3 @@ db.sync({ force: eraseDatabaseOnSync }).then(async () => {
   }
   server.listen(PORT, () => console.log(`server is running at ${PORT}`));
 });
-
-/** Example - SEED DATABASE USER (Only once) */
-// const createUsersWithMessages = async () => {
-//   await models.User.create(
-//     {
-//       username: 'rwieruch',
-//       messages: [
-//         {
-//           text: 'Published the Road to learn React'
-//         }
-//       ]
-//     },
-//     {
-//       include: [models.Message]
-//     }
-//   );
-//   await models.User.create(
-//     {
-//       username: 'ddavids',
-//       messages: [
-//         {
-//           text: 'Happy to release ...'
-//         },
-//         {
-//           text: 'Published a complete ...'
-//         }
-//       ]
-//     },
-//     {
-//       include: [models.Message]
-//     }
-//   );
-// };
